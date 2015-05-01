669fd23e8a28c46bdde56719d47a297a3c2a264e
mygithubid <joel.kniaz@gmail.com>
2015-05-01 15:23:28 -0400

    Adding synchronization to CommonMenusServices cache access static methods

--- otf-util/src/main/java/gov/va/isaac/util/CommonMenusServices.java
+++ otf-util/src/main/java/gov/va/isaac/util/CommonMenusServices.java
@@ -2,6 +2,7 @@ package gov.va.isaac.util;
 
 import gov.va.isaac.util.CommonMenus.CommonMenuItem;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,21 +13,21 @@ class CommonMenusServices {
 		T executeServiceCall();
 	}
 
-	private static final Map<CommonMenus.CommonMenuItem, ServiceCall<?>> serviceCallCache = new HashMap<>();
+	private static final Map<CommonMenus.CommonMenuItem, ServiceCall<?>> serviceCallCache = Collections.synchronizedMap(new HashMap<>());
 
-	static void setServiceCall(CommonMenuItem item, ServiceCall<?> serviceCall) {
+	static synchronized void setServiceCall(CommonMenuItem item, ServiceCall<?> serviceCall) {
 		serviceCallCache.put(item, serviceCall);
 	}
 
-	static CommonMenusServices.ServiceCall<?> getServiceCall(CommonMenuItem item) {
+	static synchronized CommonMenusServices.ServiceCall<?> getServiceCall(CommonMenuItem item) {
 		return CommonMenusServices.serviceCallCache.get(item);
 	}
 
 	// Cache for availability of services for respective CommonMenuItem
-	private static final Map<CommonMenuItem, Boolean> serviceAvailabilityCache = new HashMap<>();
+	private static final Map<CommonMenuItem, Boolean> serviceAvailabilityCache = Collections.synchronizedMap(new HashMap<>());
 
 	// Initialize and/or get service availability for CommonMenuItem
-	static boolean isServiceAvailable(CommonMenuItem item) {
+	static synchronized boolean isServiceAvailable(CommonMenuItem item) {
 		if (serviceAvailabilityCache.get(item) == null) {
 			if (getServiceCall(item) == null) {
 				serviceAvailabilityCache.put(item, false);
