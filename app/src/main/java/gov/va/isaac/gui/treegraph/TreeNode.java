0f74ae62fd1828fea33897e3b4e25bca8857f02f
Joel Kniaz <joel.kniaz@gmail.com>
2015-08-13 16:25:23 -0400

    Changeing TreeNode to take Region instead of Node

--- app/src/main/java/gov/va/isaac/gui/treegraph/TreeNode.java
+++ app/src/main/java/gov/va/isaac/gui/treegraph/TreeNode.java
@@ -1,18 +1,11 @@
 package gov.va.isaac.gui.treegraph;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javafx.scene.Group;
-import javafx.scene.shape.Shape;
+import javafx.scene.layout.Region;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.Node;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.ReadOnlyListWrapper;
-import javafx.collections.ObservableList;
 
 public class TreeNode extends Group {
 	static class Coordinates {
@@ -21,17 +14,13 @@ public class TreeNode extends Group {
 		
 		public Coordinates(double x, double y) { this.x = x; this.y = y; }
 		
-		public double getX() {
-			return x;
-		}
-		public double getY() {
-			return y;
-		}
+		public double getX() { return x; }
+		public double getY() { return y; }
 		
 		public String toString() { return "(" + x + ", " + y + ")"; }
 	}
 
-	private final static double preferredWidth = 50;
+	private final static double preferredWidth = 100;
 	private final static double preferredHeight = 25;
 	
 	private final static double vertSpaceBetweenChildNodes = 10;
@@ -40,36 +29,36 @@ public class TreeNode extends Group {
 	final TreeNode parentTreeNode;
 	TreeNode childToRight;
 	
-	final Node fxNode;
+	final Region fxNode;
 	final List<TreeNode> childTreeNodes = new ArrayList<TreeNode>();
 	
-	public TreeNode(TreeNode parentTreeNode, Node fxNode) {
+	public TreeNode(TreeNode parentTreeNode, Region fxNode) {
 		super(fxNode);
-
-		fxNode.prefHeight(preferredHeight);
-		fxNode.prefWidth(preferredWidth);
-		fxNode.maxHeight(preferredHeight);
-		fxNode.maxWidth(preferredWidth);
-		fxNode.minHeight(preferredHeight);
-		fxNode.minWidth(preferredWidth);
-
+		
 		fxNode.setStyle("-fx-border-color: black;");
+		
+		fxNode.setMaxHeight(preferredHeight);
+		fxNode.setMaxWidth(preferredWidth);
+		fxNode.setPrefHeight(preferredHeight);
+		fxNode.setPrefWidth(preferredWidth);
+		fxNode.setMinHeight(preferredHeight);
+		fxNode.setMinWidth(preferredWidth);
 		
 		this.parentTreeNode = parentTreeNode;
 		this.fxNode = fxNode;
 	}
 	
 	Coordinates getRightConnectionPortCoordinates() {
-		return new Coordinates(fxNode.getLayoutX() + preferredWidth, fxNode.getLayoutY() + preferredHeight / 2);
+		return new Coordinates(fxNode.getLayoutX() + fxNode.getMaxWidth(), fxNode.getLayoutY() + fxNode.getMaxHeight() / 2);
 	}
 	Coordinates getLeftConnectionPortCoordinates() {
-		return new Coordinates(fxNode.getLayoutX(), fxNode.getLayoutY() + preferredHeight / 2);
+		return new Coordinates(fxNode.getLayoutX(), fxNode.getLayoutY() + fxNode.getMaxHeight() / 2);
 	}
 	Coordinates getBottomConnectionPortCoordinates() {
-		return new Coordinates(fxNode.getLayoutX() + preferredWidth / 2, fxNode.getLayoutY() + preferredHeight);
+		return new Coordinates(fxNode.getLayoutX() + fxNode.getMaxWidth() / 2, fxNode.getLayoutY() + fxNode.getMaxHeight());
 	}
 	
-	public Node getFxNode() {
+	public Region getFxNode() {
 		return fxNode;
 	}
 	
@@ -79,19 +68,19 @@ public class TreeNode extends Group {
 	public void setChildToRight(TreeNode treeNode) {
 		childToRight = treeNode;
 		
-		childToRight.fxNode.setLayoutX(fxNode.getLayoutX() + preferredWidth + horizontalSpaceBetweenParentAndChildNodes);
+		childToRight.fxNode.setLayoutX(fxNode.getLayoutX() + fxNode.getMaxWidth() + horizontalSpaceBetweenParentAndChildNodes);
 		childToRight.fxNode.setLayoutY(fxNode.getLayoutY());
 
 		this.getChildren().add(childToRight);
 		
 		Coordinates startCoordinates = this.getRightConnectionPortCoordinates();
 		Coordinates endCoordinates = childToRight.getLeftConnectionPortCoordinates();
-		Line line = new Line(fxNode.getLayoutX() + preferredWidth, fxNode.getLayoutY() + preferredHeight / 2, childToRight.fxNode.getLayoutX(), childToRight.fxNode.getLayoutY() + preferredHeight / 2);
-		getChildren().add(line);
+		Line line = new Line(fxNode.getLayoutX() + fxNode.getMaxWidth(), fxNode.getLayoutY() + fxNode.getMaxHeight() / 2, childToRight.fxNode.getLayoutX(), childToRight.fxNode.getLayoutY() + preferredHeight / 2);
+		childToRight.getChildren().add(line);
 	}
 	
 	public double getHeight() {
-		double height = preferredHeight;
+		double height = fxNode.getMaxHeight();
 		
 		for (TreeNode treeNode : childTreeNodes) {
 			height = height + vertSpaceBetweenChildNodes + treeNode.getHeight();
@@ -100,7 +89,7 @@ public class TreeNode extends Group {
 	}
 
 	public double getWidth() {
-		return childToRight == null ? preferredHeight : preferredHeight + horizontalSpaceBetweenParentAndChildNodes + childToRight.getWidth();
+		return childToRight == null ? fxNode.getMaxWidth() : fxNode.getMaxWidth() + horizontalSpaceBetweenParentAndChildNodes + childToRight.getWidth();
 	}
 	
 	public TreeNode getParentTreeNode() { return parentTreeNode; }
@@ -110,7 +99,7 @@ public class TreeNode extends Group {
 	}
 
 	public void addChildTreeNodeBelow(TreeNode childTreeNode) {
-		double childLayoutX = fxNode.getLayoutX() + preferredWidth + horizontalSpaceBetweenParentAndChildNodes; // just an arbitrary indent
+		double childLayoutX = fxNode.getLayoutX() + fxNode.getMaxWidth() + horizontalSpaceBetweenParentAndChildNodes; // just an arbitrary indent
 		double childLayoutY = fxNode.getLayoutY() + getHeight() + vertSpaceBetweenChildNodes;
 		
 		childTreeNode.getFxNode().setLayoutX(childLayoutX);
@@ -121,8 +110,7 @@ public class TreeNode extends Group {
 		Coordinates endCoordinates = childTreeNode.getLeftConnectionPortCoordinates();
 		Line verticalLine = new Line(startCoordinates.getX(), startCoordinates.getY(), startCoordinates.getX(), endCoordinates.getY());
 		Line horizontalLine = new Line(startCoordinates.getX(), endCoordinates.getY(), endCoordinates.getX(), endCoordinates.getY());
-		getChildren().addAll(verticalLine, horizontalLine);
-
+		childTreeNode.getChildren().addAll(verticalLine, horizontalLine);
 		
 		this.getChildren().add(childTreeNode);
 		childTreeNodes.add(childTreeNode);
