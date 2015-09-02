2f3fbf366ccb4bdd3cc85ab8fba03b307b93d3ec
Joel Kniaz <joel.kniaz@gmail.com>
2015-09-02 16:55:31 -0400

    Changed column names to conform to BRD

--- otf-util/src/main/java/gov/va/isaac/gui/treegraph/TreeNodeUtils.java
+++ otf-util/src/main/java/gov/va/isaac/gui/treegraph/TreeNodeUtils.java
@@ -1,14 +1,11 @@
 package gov.va.isaac.gui.treegraph;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javafx.scene.layout.Region;
 import javafx.scene.control.Labeled;
 import javafx.scene.text.Font;
 
 public class TreeNodeUtils {
-	private static final Logger LOG = LoggerFactory.getLogger(TreeNodeUtils.class);
+	//private static final Logger LOG = LoggerFactory.getLogger(TreeNodeUtils.class);
 
 	private TreeNodeUtils() {};
 	
